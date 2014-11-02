af994ff9c3f5e84775e5a03a3d6723f979c506d7
Dufrene Julien <dufren_a@epita.fr>
2014-11-02 21:56:22 +0100

    Contrat o/

--- initialisation_contrat.c
+++ initialisation_contrat.c
@@ -4,32 +4,32 @@ void initialisation_contrat (t_joueur* joueur,t_contrat* contrat1)
 {
     if (joueur->niveau == 1)
     {
-        contrat1->soleils = 20;
-        contrat1->fraises = 15;
-        contrat1->pommes = 15;
+        contrat1->soleils = 60;
+        contrat1->fraises = 50;
+        contrat1->pommes = 55;
         contrat1->oignons = 0;
-        contrat1->mandarines = 5;
+        contrat1->mandarines = 50;
         contrat1->coups = 15;
     }
 
     if (joueur->niveau == 2)
     {
-        contrat1->soleils = 0;
-        contrat1->fraises = 2;
-        contrat1->pommes = 45;
-        contrat1->oignons = 12;
+        contrat1->soleils = 50;
+        contrat1->fraises = 65;
+        contrat1->pommes = 50;
+        contrat1->oignons = 55;
         contrat1->mandarines = 0;
-        contrat1->coups = 15;
+        contrat1->coups = 20;
     }
 
     if (joueur->niveau == 3)
     {
-        contrat1->soleils = 0;
-        contrat1->fraises = 30;
-        contrat1->pommes = 25;
-        contrat1->oignons = 35;
-        contrat1->mandarines = 30;
-        contrat1->coups = 0;
-    }
+        contrat1->soleils = 65;
+        contrat1->fraises = 70;
+        contrat1->pommes = 60;
+        contrat1->oignons =70;
+        contrat1->mandarines = 60;
+        contrat1->coups = 25;
 
+    }
 }
